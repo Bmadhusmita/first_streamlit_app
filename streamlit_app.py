b8bc955e5ae1f55b3680c980a14835c4891c5529
Bmadhusmita <121278138+Bmadhusmita@users.noreply.github.com>
2023-04-27 16:02:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
-streamlit.title('Breakfast menu')
-streamlit.title('omega 3 and blueberry')
-streamlit.title('hard boiled and free range')
+streamlit.header('Breakfast menu')
+streamlit.body('omega 3 and blueberry')
+streamlit.body('hard boiled and free range')
